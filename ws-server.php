5d9e64e27063ac54e13dcee8dd1c6acdbe110fb6
Vertex <laironcs@gmail.com>
2026-05-05 22:05:57 +0300

    ws-server.php

--- ws-server.php
+++ ws-server.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 const WS_HOST = '0.0.0.0';
 const WS_PORT = 8081;
-const BACKEND_HOST = '127.0.0.1';
+const BACKEND_HOST = '0.0.0.0';
 const BACKEND_PORT = 8091;
-const EVENT_LOG_PATH = __DIR__ . '/data/ws_events.log';
 
 set_time_limit(0);
 error_reporting(E_ALL);
@@ -36,8 +35,6 @@ stream_set_blocking($backendServer, false);
 $clients = [];
 $socketToClient = [];
 $backendPeers = [];
-$lastLogOffset = is_file(EVENT_LOG_PATH) ? filesize(EVENT_LOG_PATH) : 0;
-$lastLogCheckAt = microtime(true);
 
 echo "Pulse WS server listening on ws://" . WS_HOST . ':' . WS_PORT . PHP_EOL;
 echo "Backend event channel on tcp://" . BACKEND_HOST . ':' . BACKEND_PORT . PHP_EOL;
@@ -101,11 +98,6 @@ while (true) {
         }
     }
 
-    $now = microtime(true);
-    if (($now - $lastLogCheckAt) >= 0.5) {
-        $lastLogCheckAt = $now;
-        $lastLogOffset = processLogEvents($lastLogOffset, $clients);
-    }
 }
 
 function handleClientRead(array &$clients, array &$socketToClient, int $id): void
@@ -124,20 +116,27 @@ function handleClientRead(array &$clients, array &$socketToClient, int $id): voi
 
     if (!$clients[$id]['handshake_done']) {
         $clients[$id]['handshake_buffer'] .= $chunk;
-        if (strpos($clients[$id]['handshake_buffer'], "\r\n\r\n") === false) {
+        $headerEndPos = strpos($clients[$id]['handshake_buffer'], "\r\n\r\n");
+        if ($headerEndPos === false) {
             return;
         }
 
-        if (!performHandshake($sock, $clients[$id]['handshake_buffer'])) {
+        $rawHandshake = substr($clients[$id]['handshake_buffer'], 0, $headerEndPos + 4);
+        $remaining = substr($clients[$id]['handshake_buffer'], $headerEndPos + 4);
+        if (!performHandshake($sock, $rawHandshake)) {
             disconnectClient($clients, $socketToClient, $id);
             return;
         }
         $clients[$id]['handshake_done'] = true;
         $clients[$id]['handshake_buffer'] = '';
-        return;
+        if ($remaining !== '') {
+            $clients[$id]['buffer'] .= $remaining;
+        }
+    } else {
+        $clients[$id]['buffer'] .= $chunk;
     }
 
-    $messages = decodeWsFrames($chunk);
+    $messages = decodeWsFramesFromBuffer($clients[$id]['buffer']);
     foreach ($messages as $message) {
         processClientMessage($clients, $id, $message);
     }
@@ -170,43 +169,6 @@ function handleBackendRead(array &$backendPeers, int $peerId, array &$clients): 
             dispatchEvent($clients, $event);
         }
     }
-}
-
-function processLogEvents(int $offset, array &$clients): int
-{
-    if (!is_file(EVENT_LOG_PATH)) {
-        return 0;
-    }
-
-    $size = filesize(EVENT_LOG_PATH);
-    if ($size === false) {
-        return $offset;
-    }
-    if ($size < $offset) {
-        $offset = 0;
-    }
-    if ($size === $offset) {
-        return $offset;
-    }
-
-    $fp = fopen(EVENT_LOG_PATH, 'rb');
-    if ($fp === false) {
-        return $offset;
-    }
-    fseek($fp, $offset);
-    while (($line = fgets($fp)) !== false) {
-        $line = trim($line);
-        if ($line === '') {
-            continue;
-        }
-        $event = json_decode($line, true);
-        if (is_array($event)) {
-            dispatchEvent($clients, $event);
-        }
-    }
-    $newOffset = ftell($fp);
-    fclose($fp);
-    return is_int($newOffset) ? $newOffset : $offset;
 }
 
 function processClientMessage(array &$clients, int $clientId, string $raw): void
@@ -319,7 +281,7 @@ function performHandshake($socket, string $request): bool
     return @fwrite($socket, $response) !== false;
 }
 
-function decodeWsFrames(string $buffer): array
+function decodeWsFramesFromBuffer(string &$buffer): array
 {
     $messages = [];
     $offset = 0;
@@ -372,6 +334,10 @@ function decodeWsFrames(string $buffer): array
         }
 
         $offset += $frameSize;
+    }
+
+    if ($offset > 0) {
+        $buffer = (string) substr($buffer, $offset);
     }
 
     return $messages;
